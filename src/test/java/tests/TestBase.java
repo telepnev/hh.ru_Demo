bf83ddccbadbcb9c52b94f84d9c420f994a08af7
Evgeniy Telepnev <telepnef@yandex.ru>
2021-01-12 22:43:43 +0300

    init

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,7 +4,7 @@ import com.codeborne.selenide.Configuration;
 import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
@@ -13,29 +13,29 @@ import static helpers.AttachmentHelper.*;
 
 
 public class TestBase {
-    @BeforeAll
-    static void setup() {
-        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+    @BeforeEach
+    public void setup() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true)
+                .savePageSource(true));
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
-        if(System.getProperty("remote.browser.url") != null)
-            Configuration.remote = "https://user1:1234@" + System.getProperty("remote.browser.url") + ":4444/wd/hub/";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
+        //Configuration.remote = "http://localhost:4444/wd/hub/";
         Configuration.startMaximized = true;
+        Configuration.timeout = 6000;
     }
 
     @AfterEach
     @Step("Attachments")
-    public void afterEach(){
+    public void afterEach() {
         attachScreenshot("Last screenshot");
         attachPageSource();
         attachAsText("Browser console logs", getConsoleLogs());
-        if(System.getProperty("remote.browser.url") != null)
-            attachVideo();
-
+        attachVideo();
         closeWebDriver();
     }
 }
